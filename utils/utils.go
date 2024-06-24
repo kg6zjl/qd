07f8f180cad9f23fdd1576eb63deb541f644f759
Steve <13557887+kg6zjl@users.noreply.github.com>
2024-06-24 16:52:56 -0700

    short name and sanitize

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,9 @@ package utils
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,13 +17,39 @@ func UniqName(base string) string {
 	randInt := rand.Int()
 	uniqueId := strconv.Itoa(randInt)
 
+	// get short name (63 chars and limited special chars)
+	shortBase := short_name(base)
+
 	// verify the unique bit is at least 12 chars or use the whatever we've got
 	if len(uniqueId) > 8 {
 		uniqueId = uniqueId[:8]
 	}
 
 	// format the name
-	uniqueName := fmt.Sprintf("%s-qd-%s", base, uniqueId)
+	uniqueName := fmt.Sprintf("%s-qd-%s", shortBase, uniqueId)
 
 	return uniqueName
+}
+
+func short_name(base string) string {
+	var shortStr string
+	// Check if "/" is in the string
+	if strings.Contains(base, "/") {
+		// Split the string on "/"
+		splitStr := strings.Split(base, "/")
+		shortStr = splitStr[1]
+	} else {
+		shortStr = base
+	}
+
+	// Santize down to allowed chars
+	reg, _ := regexp.Compile("[^a-zA-Z0-9-_.]+")
+	shortStr = reg.ReplaceAllString(shortStr, "")
+
+	// Shorten it to 15 characters
+	if len(shortStr) > 15 {
+		shortStr = shortStr[:15]
+	}
+
+	return shortStr
 }
